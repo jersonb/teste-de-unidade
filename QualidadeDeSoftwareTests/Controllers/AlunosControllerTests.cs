4afd2323232483bbca72d6ebf18e6dd65b9fe613
jersonb <jersonlbrito@hotmail.com>
2023-11-04 08:34:57 -0300

    wip

---
+++ QualidadeDeSoftwareTests/Controllers/AlunosControllerTests.cs
@@ -0,0 +1,18 @@
+﻿using Xunit;
+
+namespace QualidadeDeSoftware.Controllers.Tests
+{
+    public class AlunosControllerTests
+    {
+        [Fact]
+        public void IndexTest()
+        {
+            Assert.True(false, "This test needs an implementation");
+        }
+
+        [Fact()]
+        public void IndexTest1()
+        {
+        }
+    }
+}
